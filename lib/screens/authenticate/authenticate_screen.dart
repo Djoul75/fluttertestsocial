bebf1516a17ed042e86bb7caa85f9259119700ba
Jules Gorce <jules.gorce@gmail.com>
2022-08-22 12:03:49 +0200

    package ajoutés (firebase_auth, cloud_firestore, provider & flutter_spinkit) et dossier screen créé

---
+++ lib/screens/authenticate/authenticate_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthenticateScreen extends StatefulWidget {
+  const AuthenticateScreen({super.key});
+
+  @override
+  State<AuthenticateScreen> createState() => _AuthenticateScreenState();
+}
+
+class _AuthenticateScreenState extends State<AuthenticateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
